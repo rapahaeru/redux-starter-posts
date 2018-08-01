64f630f16aff0b2e509e9e0f19ff32722a0dc66c
raphael.oliveira <rapahaeru@gmail.com>
2018-08-01 15:01:06 -0300

    evolui o componente Field

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostNew extends Component {
+
+    // o argumento field é um 'event handler' do componente Field
+    // onde é chamado. para podermos ter os controles necessários
+    // dos elementos do form
+    renderTitleField(field) {
+        return(
+            <div>
+                {/* // onChange, onBlur , Onclick
+                <input onFocus={field.input.onFocus} />
+                <input onChange={field.input.onChange} /> */}
+                <input type="text" {...field.input} />
+            </div>
+        );
+    }
+
     render() {
         return(
             <form>
                 <Field
                     name="title"
-                    component={}
+                    component={ this.renderTitleField}
                 />
             </form>
         );
