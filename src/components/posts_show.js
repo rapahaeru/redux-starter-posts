2aaad5f7bd5154f0d9ae394473d47280056e1c41
raphael.oliveira <rapahaeru@gmail.com>
2018-08-02 16:44:47 -0300

    Implementa toda parte de consulta direta de um post previamente inserido

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
 
+class PostsShow extends Component {
 
-class PostShow extends Component {
+    componentDidMount() {
+        // o parametro match é provido do Router
+        // esse id é o valor do :id fornecido no router '/posts:id'
+        const id = this.props.match.params.id
+        this.props.fetchPost(id);
+    }
+
     render() {
+
+        const { post } = this.props;
+
+        if (!post) {
+            return <div>loading...</div>;
+        }
+
         return (
             <div>
-                Post Show!
+                <h3>{ post.title }</h3>
+                <h6>Categories: { post.categories }</h6>
+                <p>{ post.content }</p>
             </div>
         );
     }
 }
 
-export default PostShow;
+
+// o segundo argumento, ownProps é o espelho do "this.props"
+// do componente acima. this.props = ownPosts.
+// Dessa forma conseguimos pegar o exato post que desejamos
+// bem aqui no 'mapStateToProps', Ao invés de enviar todos
+// e percorrer dentro do componente
+export function mapStateToProps({ posts }, ownProps) {
+    return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,18 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions/index';
+import { FETCH_POSTS, FETCH_POST } from '../actions/index';
 
 export default function(state = {}, action) {
 
     switch(action.type) {
+        case FETCH_POST:
+            const post = action.payload.data;
+            // const newState = { ...state };
+            // newState[post.id] = post;
+            // return newState;
+
+            // o mesmo código em ES6
+            return { ...state, [post.id]: post };
+
         case FETCH_POSTS:
             // através desse método do lodash, ele pegar uma lista de objetos e consegue exportar um
             // dos itens como indice, facilitando a pesquisa, no caso, foi escolhido o id
