fcbe5c854cd45326d0b24ea5effac4f09317b354
raphael.oliveira <rapahaeru@gmail.com>
2018-08-02 17:05:29 -0300

    Adiciona componente Link na lista de posts e adiciona bota ode voltar para o indice no detalhe do psot

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions/index';
+import { Link } from 'react-router-dom';
 
 class PostsShow extends Component {
 
@@ -21,6 +22,7 @@ class PostsShow extends Component {
 
         return (
             <div>
+                <Link to="/" >De volta para o indice</Link>
                 <h3>{ post.title }</h3>
                 <h6>Categories: { post.categories }</h6>
                 <p>{ post.content }</p>
