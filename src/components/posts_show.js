aa00d72039ad6a3d1125eb93d6a7b5e2a8de7ff8
raphael.oliveira <rapahaeru@gmail.com>
2018-08-02 15:08:06 -0300

    implementa nova rota e tela para exibir conteúdo do post

---
+++ src/components/posts_show.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class PostShow extends Component {
+    render() {
+        return (
+            <div>
+                Post Show!
+            </div>
+        );
+    }
+}
+
+export default PostShow;
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import PostIndex from './components/posts_index';
 import PostNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -17,6 +18,9 @@ ReactDOM.render(
 		<div>
 			<Switch>
 				<Route path="/posts/new" component={ PostNew } />
+				{/* A rota abaixo deve se manter sempre após o '/new', pois a rota pode interpretar
+				com se o :id fosse o 'new' */}
+				<Route path="/posts/:id" component={ PostsShow } />
 				<Route path="/" component={ PostIndex } />
 			</Switch>
 		</div>
